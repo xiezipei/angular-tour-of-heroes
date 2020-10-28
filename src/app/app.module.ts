745e52aa2d9b7276cf0d122f677a0f42a0d46741
xiezipei <xiezipei@zckd>
2020-10-28 23:27:53 +0800

    update: 将组件替换为 ng-zorro 组件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,32 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { zh_CN } from 'ng-zorro-antd/i18n';
+
+// 按钮
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
+// 菜单
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+
+// 引入图标
+import { IconDefinition } from '@ant-design/icons-angular';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+// 全部引入（不推荐）
+// import * as AllIcons from '@ant-design/icons-angular/icons';
+// const antDesignIcons = AllIcons as {
+//   [key: string]: IconDefinition;
+// };
+// const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key]);
+// 按需引入（推荐）
+import { PieChartOutline, UnorderedListOutline } from '@ant-design/icons-angular/icons';
+const icons: IconDefinition[] = [ PieChartOutline, UnorderedListOutline ];
+
+// 间距
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+
+// 布局
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -31,6 +57,11 @@ registerLocaleData(zh);
     HeroSearchComponent,
   ],
   imports: [
+    NzButtonModule,
+    NzMenuModule,
+    NzIconModule.forRoot(icons),
+    NzSpaceModule,
+    NzLayoutModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
